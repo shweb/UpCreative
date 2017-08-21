92cfa62d1d2c0b6c9fd5d20a6fe1137608b81b0a
hasiina <hasiina53@gmail.com>
2017-08-21 05:13:36 -0800

    popupintegrate

--- wp-content/themes/upcreative/enquire.php
+++ wp-content/themes/upcreative/enquire.php
@@ -1,40 +1,50 @@
-	<div id="myModal" class="modal fade" role="dialog">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">&times;</button>
-					<?php if(ICL_LANGUAGE_CODE == 'en'): ?>
-					<h1 class="modal-title">Contact us</h1>
-					<?php else: ?>
-					<h1 class="modal-title">联系我们</h1>
-					<?php endif; ?>
-					<h1 class="modal-title2">0086 15821835428</h1>
-				</div>
-				<div class="modal-body">
-					<?php if(ICL_LANGUAGE_CODE == 'en'): ?>
-					<?php echo do_shortcode('[contact-form-7 id="35" title="Contact form 1"]'); ?>	
-					<?php else: ?>
-					<?php echo do_shortcode('[contact-form-7 id="89" title="Contact form chinese"]'); ?>
-					<?php endif; ?>
-				</div>
-				<div class="modal-footer">
-					<div class="row">
-						<div class="col-md-12 ">
-							<div class="col-md-3 col-lg-3 col-xs-6 col-sm-3">
-								<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/fb.png" alt="facebook"></a>
-							</div>
-							<div class="col-md-3  col-lg-3 col-xs-6 col-sm-3">
-								<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/goo.png" alt="google"></a>
-							</div>
-							<div class="col-md-3  col-lg-3 col-xs-6 col-sm-3">
-								<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/in.png" alt="inquare"></a>
-							</div>
-							<div class="col-md-3  col-lg-3 col-xs-6 col-sm-3">
-								<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/tw.png" alt="tweeter"></a>
-							</div>
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h1 class="modal-title">Contact us</h1>
+		<p class="modal-title2">032514547585</p>
+      </div>
+      <div class="modal-body">
+		<form>
+			<div class="form-group">
+			<input type="text" class="form-control placehold" id="input-name"  placeholder="Name">
+			</div>
+			<div class="form-group">
+			<input type="e-mail" class="form-control placehold" id="input-mail" placeholder="E-mail">
+			</div>
+			<div class="form-group">
+			<input type="phone" class="form-control placehold" id="input-phone" placeholder="Phone">
+			</div>
+			<div class="form-group">
+			  <textarea class="form-control placehold-text" rows="5" id="input-message" placeholder="Your message"></textarea>
+			</div> 
+
+			<button class="pull-right btn-sender" type="submit" class="btn ">Send</button>
+		</form>
+      </div>
+      <div class="modal-footer">
+	<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="col-md-3 col-lg-3 col-xs-6 col-sm-3">
+							<img id="social" src="<?php echo get_template_directory_uri(); ?>/img/fb.png" alt="facebook">
+						</div>
+						<div class="col-md-3  col-lg-3 col-xs-6 col-sm-3">
+							<img  id="social" src="<?php echo get_template_directory_uri(); ?>/img/goo.png" alt="google">
+						</div>
+						<div class="col-md-3  col-lg-3 col-xs-6 col-sm-3">
+							<img id="social" src="<?php echo get_template_directory_uri(); ?>/img/in.png" alt="inquare">
+						</div>
+						<div class="col-md-3  col-lg-3 col-xs-6 col-sm-3">
+							<img id="social" src="<?php echo get_template_directory_uri(); ?>/img/tw.png" alt="tweeter">
 						</div>
 					</div>
-				 </div>
-			</div>
-		</div>
-	</div>
+					</div>
+      </div>
+    </div>
+
+  </div>
+</div>
