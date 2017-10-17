47d7d0e4ea1eeb5b2be2663b5788acfbc0972dba
hasiina <hasiina53@gmail.com>
2017-10-17 10:00:39 +0100

    template3

---
+++ wp-content/themes/upcreative/template-androiddev.php
@@ -0,0 +1,29 @@
+<?php /* Template Name: iosappli Page Template */ ?>
+<?php get_header(); ?>
+	
+    <!-- section about upcreative start -->
+    <div class="mainbar-spacer white"></div>
+	
+	<div class="bg-service">
+		<div class="container">
+			<div class="row service-content">
+				<section>
+					<div class="row">
+						<div class="col-md-5">
+							<div class="image round fit">
+							<img src="<?php echo get_template_directory_uri(); ?>img/androiddev.png"/>
+							</div>
+						</div>
+						<div class="col-md-7">
+							<h1 class="title-detailservice"><?php the_title(); ?></h1>
+							<p class="text-service">we always work on latest iOS app development technologies and trends to provide future-ready mobile apps. Our iPhone app development company always follow agile development methodology for shortened development lifecycles and faster project completion.</p>
+							<a href="<?php  get_site_url; ?>services"><button class="btn btn-primary btn-service btn-lg pull-right">Previous</button></a>
+						</div>
+					</div>
+				</section>
+			</div>
+		</div>
+	</div>
+		
+	<?php get_footer(); ?>
+
--- wp-content/themes/upcreative/template-hybrid.php
+++ wp-content/themes/upcreative/template-hybrid.php
@@ -17,7 +17,7 @@
 						<div class="col-md-7">
 						<h1 class="title-detailservice"><?php the_title(); ?></h1>
 						<p>With our expert hybrid app development services, we create a perfect blend of native apps usability and web app versatility. Make UpCreative your hybrid app development partner and unleash new horizons of opportunities.If you want your mobile application to be available in the market fast and within limited budget and maintenance cost, you should go for hybrid mobile app development service.</p>
-							<a href ="http://localhost/UpCreative/service/"><button class="btn btn-primary btn-service btn-lg pull-right">Previous</button></a>
+							<a href="<?php  get_site_url; ?>services"><button class="btn btn-primary btn-service btn-lg pull-right">Previous</button></a>
 						</div>
 					</div>
 				</section>
